a4cc563bb690b194ab59c25b625337d473d6136e
aktlsmsthf <lch7224@kaist.ac.kr>
2016-09-21 02:47:04 +0900

    Update thread.h

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -87,7 +87,8 @@ struct thread
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
-    int priority;                       /* Priority. */
+    int priority[];                     /* Priority. */
+    int priority_pointer;               /* PP */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
