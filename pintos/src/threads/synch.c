22dcd0c74ee4749978db3d19081e8b523d7a24b3
aktlsmsthf <lch7224@kaist.ac.kr>
2016-09-21 22:22:39 +0900

    Update synch.c

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -253,7 +253,7 @@ lock_release (struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   if(thread_current()->donated!=NULL){
-      thread_current()->donated->donating=NULL;
+      
       thread_current()->donated=NULL;
       thread_current()->priority = lock->default_priority;
       lock->default_priority=0;
