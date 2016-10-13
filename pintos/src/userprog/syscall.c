c2089039b684cc01e8266df556855cff859e25f2
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-13 20:31:03 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -4,6 +4,8 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+#include <console.h>
+
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -17,4 +19,39 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+  
+  switch(*((int *)(f->esp))){
+    case SYS_HALT:
+      break;
+    case SYS_EXIT:
+      thread_exit();
+      break;
+    case SYS_WAIT:
+      break;
+    case SYS_CREATE:
+      break;
+    case SYS_REMOVE:
+      break;
+    case SYS_OPEN:
+      break;
+    case SYS_FILESIZE:
+      break;
+    case SYS_READ:
+      break;
+    case SYS_WRITE:
+      int fd = *((int *)(f->esp+1));
+      const void *buffer = *((void **)(f->esp+2));
+      unsigned size = *((unsigned *)(f->esp+3));
+      if(fd==1){
+        putbuf(buffer, size);
+      }
+      break;
+    case SYS_SEEK:
+      break;
+    case SYS_TELL:
+      break;
+    case SYS_CLOSE:
+      break;
+  }
 }
+
