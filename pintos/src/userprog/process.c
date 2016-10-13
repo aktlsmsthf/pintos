8e830376e30c7d2f3c4c3e6f1b040429f88f8e33
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-13 22:08:11 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -133,15 +133,18 @@ start_process (void *f_name)
   *((int *)if_.esp)=0;
    printf("4\n");
    /**intr_set_level (old_level);**/
-  sema_up(&sema);
+  
   /* Start the user process by simulating a return from an
      interrupt, implemented by intr_exit (in
      threads/intr-stubs.S).  Because intr_exit takes all of its
      arguments on the stack in the form of a `struct intr_frame',
      we just point the stack pointer (%esp) to our stack frame
      and jump to it. */
-  asm volatile ("movl %0, %%esp; jmp intr_exit" : : "g" (&if_) : "memory");
+  asm volatile ("movl %0, %%esp; jmp intr_exit" : : "g" (&if_) : "memory");'
+     printf("4.1\n");
   NOT_REACHED ();
+   printf("4.2\n");
+   sema_up(&sema);
 }
 
 /* Waits for thread TID to die and returns its exit status.  If
