141c1ccad42bcb17d7c598e6bbd596176058f5f4
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-14 03:25:02 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -5,6 +5,7 @@
 #include "threads/thread.h"
 
 #include <console.h>
+#include "threads/init.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -21,23 +22,38 @@ syscall_handler (struct intr_frame *f UNUSED)
  
   
   switch(*((int *)(f->esp))){
-    case SYS_HALT:
-      break;
+    case SYS_HALT:{
+      power_off();
+      break;}
     case SYS_EXIT:{
+      int status = *((int *)(f->esp)+1);
       thread_exit();
       break;}
-    case SYS_WAIT:
+    case SYS_EXEC:{
+      const char *cmd_line = *((char **)(f->esp)+1);
       break;
-    case SYS_CREATE:
-      break;
-    case SYS_REMOVE:
-      break;
-    case SYS_OPEN:
-      break;
-    case SYS_FILESIZE:
-      break;
-    case SYS_READ:
-      break;
+    }
+    case SYS_WAIT:{
+      pid_t pid = *((pid_t *)(f->esp)+1);
+      break;}
+    case SYS_CREATE:{
+      const char *file = *((char **)(f->esp)+1);
+      unsigned initial_size = *((unsigned *)(f->esp)+2);
+      break;}
+    case SYS_REMOVE:{
+      const char *file = *((char **)(f->esp)+1);
+      break;}
+    case SYS_OPEN:{
+      const char *file = *((char **)(f->esp)+1);
+      break;}
+    case SYS_FILESIZE:{
+      int fd = *((int *)(f->esp)+1); 
+      break;}
+    case SYS_READ:{
+      int fd = *((int *)(f->esp)+1);
+      const void *buffer = *((void **)(f->esp)+2);
+      unsigned size = *((unsigned *)(f->esp)+3);
+      break;}
     case SYS_WRITE:{
       int fd = *((int *)(f->esp)+1);
       const void *buffer = *((void **)(f->esp)+2);
@@ -47,12 +63,16 @@ syscall_handler (struct intr_frame *f UNUSED)
         return size;
       }
       break;}
-    case SYS_SEEK:
-      break;
-    case SYS_TELL:
-      break;
-    case SYS_CLOSE:
-      break;
+    case SYS_SEEK:{
+      int fd = *((int *)(f->esp)+1);
+      unsigned position = *((unsigned *)(f->esp)+2);
+      break;}
+    case SYS_TELL:{
+      int fd = *((int *)(f->esp)+1);
+      break;}
+    case SYS_CLOSE:{
+      int fd = *((int *)(f->esp)+1);
+      break;}
   }
    thread_exit ();
 }
