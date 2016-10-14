3b6bc02dc2c9341b47f4425a0e9687a0963e661a
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 07:38:37 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -147,7 +147,7 @@ process_wait (tid_t child_tid UNUSED)
 {
       struct list_elem *child = list_front(&(thread_current()->child_list));
       struct thread *child_thread;
-      printf("%d\n", (int)child_tid);
+      printf("%s\n", thread_current()->name);
       printf("%s\n", list_entry(child, struct thread, child_elem)->name);
       while(list_entry(child, struct thread, child_elem)->tid != child_tid){
         child = child->next;
