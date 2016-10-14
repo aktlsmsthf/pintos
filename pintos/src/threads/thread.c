5f5edf2bdf3a84fc4dfd12cf67928c39df78bcd0
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 05:57:37 +0900

    Update thread.c

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -205,7 +205,7 @@ thread_create (const char *name, int priority,
 
 
     list_push_front(&(thread_current()->child_list), &(t->child_elem));
-    printf("ss\n");
+    printf("%s\n", list_entry(t->child_elem, struct thread, child_elem)->namer);
 
 /**#endif**/
   return tid;
