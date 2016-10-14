81075a2cfd30de801fb314e20c92adce9b7c093c
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 01:43:24 +0900

    Update thread.h

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -95,7 +95,7 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
-    int return;
+    int ret;
     struct list *child_list;
     struct list_elem *child_elem;
     bool waited;
