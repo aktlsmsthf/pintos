3964805b03519af755b9f4c2a26187f0ea293747
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 08:07:01 +0900

    Update thread.c

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -522,11 +522,13 @@ schedule_tail (struct thread *prev)
      pull out the rug under itself.  (We don't free
      initial_thread because its memory was not obtained via
      palloc().) */
+   /*
   if (prev != NULL && prev->status == THREAD_DYING && prev != initial_thread) 
     {
       ASSERT (prev != curr);
       palloc_free_page (prev);
     }
+    */
 }
 
 /* Schedules a new process.  At entry, interrupts must be off and
