587826cb959da9fbd4c7552ae029d93c808783aa
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-16 04:53:00 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -30,6 +30,7 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+   printf("%d\n",*((int *)(f->esp)));
   switch(*((int *)(f->esp))){
     case SYS_HALT:{
       power_off();
