ea44012c6d66c68f5003cdd8e69fa03fe4b18da4
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 17:37:09 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -47,7 +47,7 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
   printf("%s\n",file_name);
-  real_file_name=strtok_r((char *)file_name," ",&save);
+  /**real_file_name=strtok_r((char *)file_name," ",&save);**/
    
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (real_file_name, PRI_DEFAULT, start_process, fn_copy);
