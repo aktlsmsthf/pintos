4a5a93afd69c110afc167750c7e6564151416415
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 18:23:49 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -57,7 +57,6 @@ process_execute (const char *file_name)
   tid = thread_create (real_file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR){
     palloc_free_page (fn_copy);
-    return -1;
   }
   return tid;
 }
