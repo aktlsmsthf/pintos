0f48377af01cec17a1cccdacd59671851e5c2672
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 18:18:48 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -55,8 +55,10 @@ process_execute (const char *file_name)
    
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (real_file_name, PRI_DEFAULT, start_process, fn_copy);
-  if (tid == TID_ERROR)
-    palloc_free_page (fn_copy); 
+  if (tid == TID_ERROR){
+    palloc_free_page (fn_copy);
+    return -1;
+  }
   return tid;
 }
 
