aaade323e6a6375eb987d0079990fd98b515ebab
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 20:51:46 +0900

    Update thread.h

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -90,6 +90,9 @@ struct thread
     int priority;                       /* Priority. */
     
     struct child * child;
+    
+    bool load_finish;
+    bool load_success;
     struct list child_list;
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
