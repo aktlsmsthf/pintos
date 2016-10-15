6ab7af6c49f1db0b47862133c9e7529928728761
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 17:45:14 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -39,7 +39,9 @@ syscall_handler (struct intr_frame *f UNUSED)
       break;}
     case SYS_EXEC:{
       const char * cmd_line = *((char **)(f->esp)+1);
+      lock_aquire(&file_lock);
       tid_t pid = process_execute(cmd_line);
+      lock_release(&file_lock);
       f->eax = pid;
       break;
     }
@@ -66,6 +68,7 @@ syscall_handler (struct intr_frame *f UNUSED)
       unsigned size = *((unsigned *)(f->esp)+3);
       break;}
     case SYS_WRITE:{
+      lock_aquire(&file_lock);
       int fd = *((int *)(f->esp)+1);
       const void *buffer = *((void **)(f->esp)+2);
       unsigned size = *((unsigned *)(f->esp)+3);
@@ -73,6 +76,7 @@ syscall_handler (struct intr_frame *f UNUSED)
         putbuf(buffer, size);
         f->eax= size;
       }
+      lock_release(&file_lock);
       break;}
     case SYS_SEEK:{
       int fd = *((int *)(f->esp)+1);
