c9b112ac18a959517e5f689b0055c7522fd351b9
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-15 16:29:49 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -53,7 +53,7 @@ process_execute (const char *file_name)
   sema_down(&sema);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
-  printf("c\n");
+  printf("c %s\n",thread_current()->name);
   return tid;
 }
 
