6f25c029cc819547b4950cf522144633b3d7d9d2
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-16 04:35:00 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -30,7 +30,7 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  if(!is_user_vaddr(f->esp)) exit(-1);
+  if(!is_user_vaddr((int *)(f->esp)) exit(-1);
   switch(*((int *)(f->esp))){
     case SYS_HALT:{
       power_off();
