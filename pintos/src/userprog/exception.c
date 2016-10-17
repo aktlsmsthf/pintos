ca2b21e878ddb538fd097dc7e7cab53aff9363d0
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-17 23:46:54 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -152,8 +152,8 @@ page_fault (struct intr_frame *f)
   user = (f->error_code & PF_U) != 0;
    
    if (not_present || (is_kernel_vaddr (fault_addr) && user)){
-      kill (f);
-    exit(-1);
+      f->eax = -1;
+      return;
    }
 
   /* To implement virtual memory, delete the rest of the function
