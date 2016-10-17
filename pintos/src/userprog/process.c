2fbcd727df8aa752d216823c071acd1b8379f493
aktlsmsthf <lch7224@kaist.ac.kr>
2016-10-17 22:36:45 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -228,10 +228,10 @@ process_exit (void)
    struct list_elem * celem;
    struct child *c;
    
-   if(curr->child->parent_exited){
+   /**if(curr->child->parent_exited){
       printf("1\n");
       palloc_free_page(curr->child);
-   }
+   }**/
    while(!list_empty(&(thread_current()->file_list))){
         felem=list_front(&(thread_current()->file_list));
         ffd=list_entry(felem, struct file_fd, elem);
