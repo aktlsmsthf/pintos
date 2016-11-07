c43245e3dbade96cdb5bb7846990253e08b17f02
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-08 00:24:29 +0900

    Update frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -1,2 +1,18 @@
 #include "vm/frame.h"
+#include <list.h>
+#include "threads/synch.h"
 
+void frame_init(void){
+  list_init(frame_table);
+  lock_init(frame_lock);
+}
+
+void frame_push(void *frame){
+  struct frame_elem f;
+  f->frame = frame;
+  list_push_front(&frame_table, &f->elem);
+}
+
+void frame_pop(void){
+  
+}
