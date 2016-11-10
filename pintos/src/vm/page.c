04fab087c1aad31cb0fe82d72d232daf556b06b0
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-11 05:42:58 +0900

    Update page.c

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -1,12 +1,12 @@
 #include "vm/page.h"
 #include <hash.h>
 
-unsigned hash_hash_func(const struct hash_elem *element, void *aux){
+static unsigned hash_hash_func(const struct hash_elem *element, void *aux){
   struct spt_entry *spte = hash_entry(element, struct spt_entry, elem);
   return hash_int((int)spte->page);
 }
 
-bool hash_less_func(const struct hash_elem *a, const struct hash_elem *b, void *aux){
+static bool hash_less_func(const struct hash_elem *a, const struct hash_elem *b, void *aux){
   struct spt_entry *sa = hash_entry(a, struct spt_entry, elem);
   struct spt_entry *sb = hash_entry(b, struct spt_entry, elem);
   return sa->page < sb->page;
