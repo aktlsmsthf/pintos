051a07f42b52f3dff05fa7c8cea29eb294738c9e
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-11 04:25:55 +0900

    Update page.c

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -1,1 +1,26 @@
+#include "vm/page.h"
+#include <hash.h>
 
+unsigned hash_hash_func(const struct hash_elem *element, void *aux){
+  struct spt_entry *spte = hash_entry(element, struct spt_entry, elem);
+  return hash_int((int)spte->page);
+}
+
+bool hash_less_func(const struct hash_elem *a, const struct hash_elem *b, void *aux){
+  struct sptentry *sa = hash_entry(elemtent, struct spt_entry, elem);
+  struct sptentry *sb = hash_entry(elemtent, struct spt_entry, elem);
+  return sa->page < sb->page;
+}
+
+void spt_init(){
+  hash_init(supple_page_table, hash_hash_func, hash_less_func);
+}
+
+void page_table_alloc(void* page){
+  struct spt_entry *spte = malloc(sizeof(struct spt_entry));
+  spte->page = page;
+  spte->is_swap = false;
+  spte->index = -1;
+  
+  hash_insert(supple_page_table, spte->elem);
+}
