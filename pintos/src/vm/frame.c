19c2e8ee2b643b7084813042e76f8e6660ad93fe
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-11 01:44:33 +0900

    Update frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -1,18 +1,24 @@
 #include "vm/frame.h"
 #include <list.h>
 #include "threads/synch.h"
+#include "threads/palloc.h"
 
 void frame_init(void){
   list_init(frame_table);
   lock_init(frame_lock);
 }
 
-void frame_push(void *frame){
+void frame_alloc(){
+  void* frame = palloc_get_page(PAL_USER);
   struct frame_entry f = malloc(struct frame_entry);
   f->frame = frame;
   list_push_front(&frame_table, &f->elem);
 }
 
 struct frame_entry* frame_pop(void){
+  
+}
+
+struct frame_entry* frame_evit(void){
   
 }
