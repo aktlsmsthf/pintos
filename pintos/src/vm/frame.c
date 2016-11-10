84127e17e19ec47a045af129eb262438d9cc4265
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-11 03:32:23 +0900

    Update frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -8,12 +8,10 @@ void frame_init(void){
   lock_init(&frame_lock);
 }
 
-void* frame_alloc(){
-  void* frame = palloc_get_page(PAL_USER);
+void frame_alloc(void* frame){
   struct frame_entry *f = malloc(sizeof(struct frame_entry));
   f->frame = frame;
   list_push_front(&frame_table, &f->elem);
-  return frame;
 }
 
 struct frame_entry* frame_pop(void){
