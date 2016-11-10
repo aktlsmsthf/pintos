358fd0ce788fad91fc2d7ec53519e248be582865
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-11 01:32:17 +0900

    Update frame.h

--- pintos/src/vm/frame.h
+++ pintos/src/vm/frame.h
@@ -4,11 +4,11 @@
 struct list frame_table;
 struct lock frame_lock;
 
-struct frame_elem{
+struct frame_entry{
   void *frame;
   struct list_elem elem;
 }
 
 void frame_init(void);
 void frame_push(void *frame);
-struct frame_elem* frame_pop(void);
+struct frame_entry* frame_pop(void);
