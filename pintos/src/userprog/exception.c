c509124b875ed177a5de4cc50423f1ac5fa7efdd
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-12 22:57:09 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -156,6 +156,10 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
+   
+   if (not_present || (is_kernel_vaddr (fault_addr) && user)){
+      exit(-1);
+   }
   
    if(not_present && fault_addr >= f->esp-32 && is_user_vaddr(fault_addr)){
       uint8_t *frame = palloc_get_page(PAL_USER);
@@ -181,9 +185,7 @@ page_fault (struct intr_frame *f)
       }
    }
    
-   if (not_present || (is_kernel_vaddr (fault_addr) && user)){
-      exit(-1);
-   }
+   
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
