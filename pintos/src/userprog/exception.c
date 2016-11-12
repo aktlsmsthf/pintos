4b279099e321a1651445d4d22e53438db02c7684
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-13 04:08:48 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -156,7 +156,16 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
-   
+   if(not_present && fault_addr >= f->esp-32 && is_user_vaddr(fault_addr)){
+      uint8_t *frame = palloc_get_page(PAL_USER);
+      frame_spt_alloc(frame,&thread_current()->spt,pg_round_down(fault_addr), true);
+      /*
+      frame_alloc(frame);
+      spt_alloc(&thread_current()->spt, pg_round_down(fault_addr));
+      */
+      install_page(pg_round_down(fault_addr), frame, true);
+      return;
+   }  
    if(user){
       struct spt_entry *spte = spte_find(pg_round_down(fault_addr));
       if(spte!=NULL){
@@ -175,22 +184,11 @@ page_fault (struct intr_frame *f)
       }
    }
   
-   if(not_present && fault_addr >= f->esp-32 && is_user_vaddr(fault_addr)){
-      uint8_t *frame = palloc_get_page(PAL_USER);
-      frame_spt_alloc(frame,&thread_current()->spt,pg_round_down(fault_addr), true);
-      /*
-      frame_alloc(frame);
-      spt_alloc(&thread_current()->spt, pg_round_down(fault_addr));
-      */
-      install_page(pg_round_down(fault_addr), frame, true);
-      return;
-   }
+ 
    
-   /*
    if (not_present || (is_kernel_vaddr (fault_addr) && user)){
       exit(-1);
    }
-*/
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
