8e6b13011bc2b41991b4fbc10f44af51e7c03717
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-12 11:00:20 +0900

    Update frame.h

--- pintos/src/vm/frame.h
+++ pintos/src/vm/frame.h
@@ -17,7 +17,7 @@ struct frame_entry{
 };
 
 void frame_init(void);
-void frame_spt_alloc(void* frame, struct hash *spt, void* page);
+void frame_alloc(void* frame);
 void * frame_evict(void);
 struct frame_entry* frame_pop(void);
 
