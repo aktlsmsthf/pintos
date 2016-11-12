3d4d4ee5c144f5221cb8cb875e15870327508698
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-12 21:58:44 +0900

    Update

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -11,22 +11,17 @@ void frame_init(void){
 
 }
 
-void * frame_spt_alloc(void * frame, struct hash * spt, void * page){
-  
-  struct spt_entry *spte = malloc(sizeof(struct spt_entry));
+void * frame_alloc(void * frame){
   struct frame_entry *fe = malloc(sizeof(struct frame_entry));
   
   if(frame==NULL){
     frame=frame_evict();
     
   }
-  spte->page = page;
-  spte->fe = fe
-  hash_insert(spt ,&spte->elem);
+  
   fe->frame = frame;
   fe->in_swap = 0;
   fe->is_free = 0;
-  fe->spte = spte;
   list_push_front(&frame_table, &fe->elem);
   return frame;
 }
@@ -40,15 +35,23 @@ void* frame_evict(void){
   struct list_elem * frame_elem = list_front(&frame_table);
   struct frame_entry * fe;
   while(!list_entry(frame_elem, struct frame_entry, elem)->is_free){
+      if(!pagedir_is_accessed(thread_current()->pagedir ,list_entry(frame_elem, struct frame_entry, elem)->spte->page)){
+        if(list_entry(frame_elem, struct frame_entry, elem)->is_free==1){
+          break;
+        }
+        else{
           list_entry(frame_elem, struct frame_entry, elem)->is_free=1;
-          frame_elem = frame_elem->next;
-          if(frame_elem->next==NULL){
-             frame_elem = list_front(&frame_table);
-          }
+        }
       }
+      frame_elem = frame_elem->next;
+      if(frame_elem->next==NULL){
+         frame_elem = list_front(&frame_table);
+      }
+  }
   fe = list_entry(frame_elem, struct frame_entry, elem);
   fe->in_swap = 1;
   fe->swap_where = swap_out(fe->frame);
+  pagedir_clear_page(thread_current()->pagedir, fe->spte->page);
   ret=fe->frame;
   fe->frame = NULL;
   return ret;  
