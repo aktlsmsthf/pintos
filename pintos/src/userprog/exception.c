20e51f88335423a515205a4a65d5bf4b8a49896b
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-12 22:46:25 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -168,15 +168,17 @@ page_fault (struct intr_frame *f)
       return;
    }
    
-   struct spt_entry *spte = spte_find(&thread_current()->spt, pg_round_down(fault_addr));
-   if(spte->fe->in_swap){
-      uint8_t *frame = frame_evict();
-      swap_in(spte->fe->swap_where, frame);
-      spte->fe->in_swap = 0;
-      spte->fe->swap_where = -1;
-      spte->fe->frame = frame;
-      install_page(pg_round_down(fault_addr), frame, true);
-      return;
+   if(is_user_vaddr(fault_addr)){
+      struct spt_entry *spte = spte_find(&thread_current()->spt, pg_round_down(fault_addr));
+      if(spte->fe->in_swap){
+         uint8_t *frame = frame_evict();
+         swap_in(spte->fe->swap_where, frame);
+         spte->fe->in_swap = 0;
+         spte->fe->swap_where = -1;
+         spte->fe->frame = frame;
+         install_page(pg_round_down(fault_addr), frame, true);
+         return;
+      }
    }
    
    if (not_present || (is_kernel_vaddr (fault_addr) && user)){
