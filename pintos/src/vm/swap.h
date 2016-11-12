685175346af882338028d2d543d4a36b4d4f7493
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-12 11:17:25 +0900

    Update swap.h

--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -2,9 +2,10 @@
 #define SWAP_H
 #include <bitmap.h>
 
-struct bitmap swap_table = bitmap_create(10);
 
-struct disk swap_disk = disk_get(1,1);
+struct bitmap * swap_table = bitmap_create(10);
+
+struct disk * swap_disk = disk_get(1,1);
 
 int swap_out(void *frame);
 
