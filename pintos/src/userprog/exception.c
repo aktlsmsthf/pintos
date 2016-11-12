b89c99b93a84c0146053c29678ad13b54edd51c9
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-13 04:03:27 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -157,7 +157,7 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
    
-   if(user&&!not_present){
+   if(user){
       struct spt_entry *spte = spte_find(pg_round_down(fault_addr));
       if(spte!=NULL){
          if(spte->fe->in_swap){
@@ -186,11 +186,11 @@ page_fault (struct intr_frame *f)
       return;
    }
    
-   
+   /*
    if (not_present || (is_kernel_vaddr (fault_addr) && user)){
       exit(-1);
    }
-
+*/
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
