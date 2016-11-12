47bd520a9dd051d48293d4e87420ee9e95e69ab6
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-12 20:47:47 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -585,7 +585,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 
       /* Get a page of memory. */
       uint8_t *kpage = palloc_get_page (PAL_USER);
-      kpage = frame_alloc(kpage);
+      frame_alloc(kpage);
       if (kpage == NULL)
         return false;
 
@@ -623,7 +623,7 @@ setup_stack (void **esp)
   bool success = false;
 
   kpage = palloc_get_page (PAL_USER | PAL_ZERO);
-  kpage = frame_alloc(kpage); 
+  frame_alloc(kpage); 
   if (kpage != NULL) 
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
