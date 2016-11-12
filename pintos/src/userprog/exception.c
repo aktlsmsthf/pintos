d89fb14e9f660c8b1b06074f99d8586313473c53
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-13 08:39:06 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -172,7 +172,7 @@ page_fault (struct intr_frame *f)
    }
    
   
-   if(not_present && is_user_vaddr(fault_addr)){
+   if(is_user_vaddr(fault_addr)){
    struct spt_entry *spte = spte_find(pg_round_down(fault_addr));
       if(spte!=NULL){
          if(spte->fe->in_swap){
