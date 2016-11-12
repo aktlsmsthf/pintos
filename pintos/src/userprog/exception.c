cb43ab5fd8eb545f9c06b70a75bc794b6367ab00
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-13 07:25:14 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -188,11 +188,12 @@ page_fault (struct intr_frame *f)
       }  
    }
   
- 
+ /*
    
    if (not_present || (is_kernel_vaddr (fault_addr) && user)){
       exit(-1);
    }
+   */
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
