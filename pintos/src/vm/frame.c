66381f79930d88adaedcb0952a614a496b30bbc5
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-13 07:13:21 +0900

    Update frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -53,7 +53,7 @@ void* frame_evict(void){
   void * ret;
   struct list_elem * frame_elem = list_front(&frame_table);
   struct frame_entry * fe;
-  while(pagedir_is_accessed(thread_current()->pagedir ,list_entry(frame_elem, struct frame_entry, elem)->spte->page)){
+  while(list_entry(frame_elem, struct frame_entry, elem)->frame==NULL || pagedir_is_accessed(thread_current()->pagedir ,list_entry(frame_elem, struct frame_entry, elem)->spte->page)){
       pagedir_set_accessed(thread_current()->pagedir ,list_entry(frame_elem, struct frame_entry, elem)->spte->page, false);
       frame_elem = frame_elem->next;
       if(frame_elem->next==NULL){
