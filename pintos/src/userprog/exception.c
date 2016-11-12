02c47eae4f3e2e09181da2a367d2112da05d8478
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-13 02:19:22 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -157,19 +157,6 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
    
-   
-  
-   if(not_present && fault_addr >= f->esp-32 && is_user_vaddr(fault_addr)){
-      uint8_t *frame = palloc_get_page(PAL_USER);
-      frame_spt_alloc(frame,&thread_current()->spt,pg_round_down(fault_addr));
-      /*
-      frame_alloc(frame);
-      spt_alloc(&thread_current()->spt, pg_round_down(fault_addr));
-      */
-      install_page(pg_round_down(fault_addr), frame, true);
-      return;
-   }
-   
    if(not_present && is_user_vaddr(fault_addr)){
       struct spt_entry *spte = spte_find(pg_round_down(fault_addr));
       if(spte!=NULL){
@@ -184,6 +171,19 @@ page_fault (struct intr_frame *f)
          }
       }
    }
+  
+   if(not_present && fault_addr >= f->esp-32 && is_user_vaddr(fault_addr)){
+      uint8_t *frame = palloc_get_page(PAL_USER);
+      frame_spt_alloc(frame,&thread_current()->spt,pg_round_down(fault_addr));
+      /*
+      frame_alloc(frame);
+      spt_alloc(&thread_current()->spt, pg_round_down(fault_addr));
+      */
+      install_page(pg_round_down(fault_addr), frame, true);
+      return;
+   }
+   
+   
    if (not_present || (is_kernel_vaddr (fault_addr) && user)){
       exit(-1);
    }
