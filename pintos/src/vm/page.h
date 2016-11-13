cad497b869ceb297472b5700cdd74b0755fa60a6
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-14 04:07:56 +0900

    Update page.h

--- pintos/src/vm/page.h
+++ pintos/src/vm/page.h
@@ -11,7 +11,7 @@ struct spt_entry{
 };
 
 void spt_init(struct hash *spt);
-struct spt_entry  spte_find(void *page);
+struct spt_entry * spte_find(void *page);
 void spt_destroy (struct hash *spt);
 /*
 void spt_alloc(struct hash *spt, void *page);
