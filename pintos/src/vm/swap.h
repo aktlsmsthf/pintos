f45575403ba2a369cc58e3ac62ab4611bef33cce
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-14 01:17:43 +0900

    Update swap.h

--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -3,10 +3,12 @@
 #include <bitmap.h>
 #include "devices/disk.h"
 #include "vm/frame.h"
+#include "threads/synch.h"
 
 
 struct bitmap *swap_table ;
 struct disk *swap_disk ;
+struct lock swap_lock;
 
 void swap_init(void);
 size_t swap_out(void *frame);
