a627604b0459cdc31df68e10c2399e0891cc3baa
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-14 00:04:11 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -165,7 +165,6 @@ page_fault (struct intr_frame *f)
    struct spt_entry *spte = spte_find(pg_round_down(fault_addr));
       if(spte!=NULL){
          if(spte->fe->in_swap){
-            msg("b");
             uint8_t *frame = palloc_get_page(PAL_USER);
             if(frame==NULL){frame=frame_evict();}
             //frame_spt_alloc(frame
@@ -182,7 +181,7 @@ page_fault (struct intr_frame *f)
       }  
    }
     if(!pass && not_present && fault_addr >= f->esp-32 && is_user_vaddr(fault_addr)){
-       msg("c");
+       prrintf("%s\n", thread_curent()->name);
       uint8_t *frame = palloc_get_page(PAL_USER);
       frame_spt_alloc(frame,&thread_current()->spt,pg_round_down(fault_addr), true);
       /*
@@ -195,7 +194,6 @@ page_fault (struct intr_frame *f)
          
    }
     if (!pass && (not_present || (is_kernel_vaddr (fault_addr) && user))){
-      msg("a");
       exit(-1);
    } 
  
