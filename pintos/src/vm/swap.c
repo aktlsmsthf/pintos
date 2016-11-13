1a0c6179b40d0bdb0f561160f09499b14599fc17
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-14 03:17:54 +0900

    Update swap.c

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -53,7 +53,7 @@ void swap_in(struct frame_entry *fe, void * frame){
   fe->in_swap = 0;
   fe->swap_where = -1;
   fe->frame = frame;
-  //install_page(fe->spte->page, frame, fe->spte->writable);
+  install_page(fe->spte->page, frame, fe->spte->writable);
   lock_release(&swap_lock);
 }
 
