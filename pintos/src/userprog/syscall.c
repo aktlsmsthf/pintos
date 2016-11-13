5c80ee87a459408918fe1b0741ca829a014b7a28
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-14 03:42:54 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -370,17 +370,6 @@ bool check_buffer(void *buffer, unsigned size){
 
 bool check_bad_ptr(struct intr_frame *f,const void * uaddr){
   
-  if(uaddr >= f->esp-32 && is_user_vaddr(uaddr)){
-      uint8_t *frame = palloc_get_page(PAL_USER);
-      frame_spt_alloc(frame,&thread_current()->spt,pg_round_down(uaddr), true);
-      /*
-      frame_alloc(frame);
-      spt_alloc(&thread_current()->spt, pg_round_down(fault_addr));
-      */
-      install_page(pg_round_down(uaddr), frame, true);
-      
-      return false;
-   }
     void * p = pagedir_get_page (thread_current()->pagedir, pg_round_down(uaddr));
     return p==NULL;
 }
