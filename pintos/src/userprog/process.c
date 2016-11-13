f45a9f292c301f65eadb7a5a6301e061fdcfdfdb
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-14 01:05:16 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -588,7 +588,9 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
       /* Get a page of memory. */
+      lock_acquire(&frame_lock);
       uint8_t *kpage = palloc_get_page (PAL_USER);
+      lock_release(&frame_lock);
       kpage = frame_spt_alloc(kpage, &thread_current()->spt, upage, writable);
       if (kpage == NULL)
         return false;
@@ -626,7 +628,9 @@ setup_stack (void **esp)
   uint8_t *kpage;
   bool success = false;
 
+  lock_acquire(&frame_lock);
   kpage = palloc_get_page (PAL_USER | PAL_ZERO);
+  lock_release(&frame_lock);
   kpage = frame_spt_alloc(kpage, &thread_current()->spt, ((uint8_t *) PHYS_BASE) - PGSIZE, true); 
    
   if (kpage != NULL) 
