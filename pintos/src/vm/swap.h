d25bac49eea8bf11f00b242671a689f7d5009816
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-13 21:28:08 +0900

    Update swap.h

--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -11,5 +11,5 @@ struct disk *swap_disk ;
 void swap_init(void);
 size_t swap_out(void *frame);
 void swap_remove(size_t index);
-void swap_in(struct spte_entry *spte,void *fault_addr);
+void swap_in(struct spt_entry *spte,void *fault_addr);
 #endif
