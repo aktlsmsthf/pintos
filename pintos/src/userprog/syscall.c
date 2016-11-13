b0dc5905de3a3cd04f276f7465486e2e69272a09
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-14 08:37:51 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -64,10 +64,11 @@ syscall_handler (struct intr_frame *f UNUSED)
         printf("a\n");
         exit(-1);}
       const char * cmd_line = *((char **)(f->esp)+1);
-      if(!user_memory((void *)cmd_line, 0)){f->eax = -1; break;}
+      if(!user_memory((void *)cmd_line, 0)){
+        printf("b\n");f->eax = -1; break;}
       if(check_bad_ptr(f,(const void *)cmd_line)){
         
-        printf("b\n");
+        printf("c\n");
         exit(-1);
       }
       /**lock_acquire(&sys_lock);**/
