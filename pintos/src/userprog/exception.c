6620cec01314144b884a2cb6cab03b19da746ee7
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-13 22:51:45 +0900

    Update exception.c

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -172,10 +172,10 @@ page_fault (struct intr_frame *f)
             swap_in(spte->fe, frame);
             pagedir_set_page (thread_current()->pagedir, pg_round_down(fault_addr), frame, spte->writable);
             install_page(spte->page, frame, spte->writable);
-            /*
+            
             uint32_t *pte;
             pte = lookup_page (thread_current()->pagedir, pg_round_down(fault_addr), false);
-            *pte |= PTE_P;*/
+            *pte |= PTE_P;
             /**spte->fe->in_swap = 0;
             spte->fe->swap_where = -1;
             spte->fe->frame = frame;**/
@@ -192,9 +192,9 @@ page_fault (struct intr_frame *f)
       */
       install_page(pg_round_down(fault_addr), frame, true);
       
-      /*uint32_t *pte2;
+      uint32_t *pte2;
       pte2 = lookup_page (thread_current()->pagedir, pg_round_down(fault_addr), false);
-      *pte2 |= PTE_P; */
+      *pte2 |= PTE_P; 
       pass=true;
          
    }
