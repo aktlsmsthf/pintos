f305371cc927585974eac3a2ed5a46c30768a4f1
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-14 20:52:33 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -166,7 +166,6 @@ syscall_handler (struct intr_frame *f UNUSED)
       
     case SYS_READ:{
       if(!user_memory(f->esp,3)){
-        printf("a\n");
         exit(-1);}
       
       int fd = *((int *)(f->esp)+1);
@@ -174,10 +173,8 @@ syscall_handler (struct intr_frame *f UNUSED)
       unsigned size = *((unsigned *)(f->esp)+3);
       
       if(!user_memory((void *)buffer, 0)){
-        printf("b\n");
         exit(-1);}
       if(check_bad_ptr(f,(const void *)buffer)){
-        printf("c\b");
         exit(-1);}
       check_buffer(buffer, size);
       int j=0;
