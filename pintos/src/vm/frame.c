0dd21f0baf8a607451f6847e56e98ffab8d05f5f
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-15 08:04:26 +0900

    Update frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -15,6 +15,7 @@ void frame_init(void){
 void frame_remove(struct frame_entry *fe){
   
   lock_acquire(&frame_lock);
+  /*
   struct list_elem *e = list_front(&frame_table);
   bool exist = false;
   while(e->next!=NULL){
@@ -26,7 +27,7 @@ void frame_remove(struct frame_entry *fe){
   }
   if(!exist){
     palloc_free_page(fe->frame);
-  }
+  }*/
   list_remove(&fe->elem);
   int i=0;
   //palloc_free_page(fe->frame);
