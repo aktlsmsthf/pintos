af5b4c14b60ba79ce53a934fee145cfed483409e
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-15 04:55:57 +0900

    Update page.h

--- pintos/src/vm/page.h
+++ pintos/src/vm/page.h
@@ -2,12 +2,14 @@
 #define PAGE_H
 
 #include <hash.h>
+#include "threads/palloc.h"
 
 struct spt_entry{
   void *page;
   struct hash_elem elem;
   struct frame_entry* fe;
   bool writable;
+  enum palloc_flages flags;
 };
 
 void spt_init(struct hash *spt);
