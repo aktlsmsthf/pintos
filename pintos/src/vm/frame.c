9b01f174d27001cf480b4f1e8cb10ca4d0d0776c
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-15 22:58:58 +0900

    Update frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -22,8 +22,8 @@ void frame_remove(struct frame_entry *fe, bool pe){
   //printf("%s %x a\n", thread_current()->name, fe->frame);
   //lock_acquire(&palloc_lock);
   
-  if(!pe){palloc_free_page(fe->frame);}
-  //palloc_free_page(fe->frame);
+  //if(!pe){palloc_free_page(fe->frame);}
+  palloc_free_page(fe->frame);
   //lock_release(&palloc_lock);
   
   pagedir_clear_page(fe->t->pagedir, fe->spte->page);
