b9726e6c9be1860a1686d5a3e2167df295854840
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-15 23:03:31 +0900

    Update swap.c

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -39,9 +39,9 @@ void* swap_out(struct frame_entry *fe, enum palloc_flags flags){
   fe->in_swap = 1;
 
   lock_acquire(&palloc_lock);
-  //palloc_free_page(fe->frame);
-  ret =memset (fe->frame, 0, PGSIZE);
-  // palloc_get_page(flags);
+  palloc_free_page(fe->frame);
+  //ret =memset (fe->frame, 0, PGSIZE);
+  palloc_get_page(flags);
   pagedir_clear_page(fe->t->pagedir, fe->spte->page);
   lock_release(&palloc_lock);
  // printf("%x %x\n", fe->frame,fe->spte->page);
