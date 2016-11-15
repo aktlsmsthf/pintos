dfc604ddb7997f1813ca7301fb0b25c137cc7328
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-16 00:05:17 +0900

    Update page.c

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -19,7 +19,6 @@ static void page_destroy_func(struct hash_elem *helem, void *aux){
   struct spt_entry *spte = hash_entry(helem, struct spt_entry,elem);
   if(spte->fe->in_swap){
     swap_remove(spte->fe->swap_where);}
-  
   frame_remove(spte->fe,*((bool*)aux));  
   free(spte);
 }
