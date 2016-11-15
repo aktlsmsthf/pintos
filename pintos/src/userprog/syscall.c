c1f00a49c9029bc3ffa8f7e2714d8e473b24e1eb
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-15 09:29:25 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -62,6 +62,7 @@ syscall_handler (struct intr_frame *f UNUSED)
       tid_t pid = process_execute(cmd_line);
       /**lock_release(&sys_lock);**/
       f->eax = pid;
+	printf("%d\n", thread_ticks());
       break;
     }
       
