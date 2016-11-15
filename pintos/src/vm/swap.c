4e7dff1871ecfd6ca7bcd438a76d9ea7048792e6
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-15 17:29:43 +0900

    Update swap.c

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -48,8 +48,8 @@ void* swap_out(struct frame_entry *fe, enum palloc_flags flags){
   list_remove(&fe->elem);
   //printf("%x\n", ret);
   //printf("%d\n", fe->spte->flags);
-  ret = palloc_get_page(flags);
-  printf("%d\n", flags);
+  ret = palloc_get_page(fe->spte->flags);
+  printf("%d\n", fe->spte->flags);
   //printf("a %x\n", ret);
   //printf("%d\n", bitmap_count(swap_table, 0, disk_size(swap_disk)/spp, 1));
   lock_release(&swap_lock);
