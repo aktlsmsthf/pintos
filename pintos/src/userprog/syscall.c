b0a8715cd4c13d003681f8d73c7f231a2960f9c0
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-15 18:09:01 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -223,6 +223,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 	    buffer_size = 0;
 	 }
       }
+      struct spt_entry *spte = spte_find(pg_round_down(buffer));
       if(!spte->writable){
 	   exit(-1);
       }
@@ -297,7 +298,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 	        buffer_size = 0;
 	      }
       }
-      struct spt_entry *spte = spte_find(pg_round_down(buffer));
+      
       
       if(fd==1){
         putbuf(buffer, size);
