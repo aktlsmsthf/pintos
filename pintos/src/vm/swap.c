9704edc2176267d471b4baffb679acd231288fa1
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-15 21:49:53 +0900

    Update swap.c

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -27,34 +27,23 @@ void swap_remove(size_t index){
 void* swap_out(struct frame_entry *fe, enum palloc_flags flags){
   void* ret;
   //printf("%x %x\n ",fe, fe->frame);
-  lock_acquire(&frame_lock);
+  lock_acquire(&swap_lock);
   size_t index = bitmap_scan_and_flip(swap_table, 0, 1, 0);
   
   int i;
   for(i=0;i<spp;i++){
     disk_write(swap_disk, index*spp+i, (uint8_t *)fe->frame+DISK_SECTOR_SIZE*i);
   }
+  lock_release(&swap_lock);
   fe->swap_where = index;
   fe->in_swap = 1;
 
-  //printf("b %x\n", fe->frame);
-  //ret=fe->frame;
-  //printf("%x %x\n",fe->frame,fe->spte->page);
 
   palloc_free_page(fe->frame);
-  //printf("a\n");
   pagedir_clear_page(fe->t->pagedir, fe->spte->page);
-  //ret=fe->frame;
-  //printf("b\n");
   fe->frame = NULL;
-  //printf("c\n");
-  //list_remove(&fe->elem);
-  //printf("%x\n", ret);
-  //printf("%d\n", fe->spte->flags);
   ret = palloc_get_page(flags);
-  //printf("a %x\n", ret);
-  //printf("%d\n", bitmap_count(swap_table, 0, disk_size(swap_disk)/spp, 1));
-  lock_release(&frame_lock);
+
   return ret;
 }
 
