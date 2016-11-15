6f5cf392e7e60e5371646d586d497e4ce8f5a7cd
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-15 14:27:18 +0900

    Update swap.c

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -36,13 +36,14 @@ void* swap_out(struct frame_entry *fe){
   fe->swap_where = index;
   fe->in_swap = 1;
   //printf("b %x\n", fe->frame);
+  
+  printf("%x %x\n",fe->frame,fe->spte->page);
   palloc_free_page(fe->frame);
   pagedir_clear_page(thread_current()->pagedir, fe->spte->page);
   //ret=fe->frame;
   fe->frame = NULL;
   //printf("%x\n", ret);
   printf("%d\n", fe->spte->flags);
-  printf("%x %x\n",fe->frame,fe->spte->page);
   ret = palloc_get_page(fe->spte->flags);
   //printf("a %x\n", ret);
   //printf("%d\n", bitmap_count(swap_table, 0, disk_size(swap_disk)/spp, 1));
