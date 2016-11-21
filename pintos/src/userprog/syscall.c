4964a4645e2fece7fe49cd13d24190ccdd19e0bf
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-22 02:24:52 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -407,10 +407,11 @@ syscall_handler (struct intr_frame *f UNUSED)
       return felem;
 }
 bool user_memory(void *esp, int n){
-  int * p;
+  /*int * p;
   p = (int *)esp + n;
   if(!is_user_vaddr((const void *) p)) {return 0;}
-  else return 1;
+  else return 1;*/
+	return 1;
 }
 
 /**bool user_memory(void *esp, int n){
