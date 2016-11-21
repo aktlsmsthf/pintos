2fd727785706e831fba8f7cbc9b280f18d67d35c
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-22 00:34:49 +0900

    Update frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -8,6 +8,26 @@
 #include "threads/thread.h"
 #include "filesys/file.h"
 #include "userprog/process.h"
+#include <debug.h>
+#include <inttypes.h>
+#include <round.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "userprog/gdt.h"
+#include "userprog/pagedir.h"
+#include "userprog/tss.h"
+#include "filesys/directory.h"
+#include "filesys/filesys.h"
+#include "threads/flags.h"
+#include "threads/init.h"
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+#include "threads/vaddr.h"
+
+#include "userprog/syscall.h"
+
+#include "vm/page.h"
 
 void frame_init(void){
   list_init(&frame_table);
