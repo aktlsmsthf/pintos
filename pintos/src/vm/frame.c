0c9de7e366818c12cb787ff410e64b1d7190e06a
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-22 16:41:44 +0900

    Update frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -108,9 +108,9 @@ bool file_frame_alloc(struct spt_entry * spte){
     return 0;
   }*/
   //lock_release(&sys_lock);
-  lock_acquire(&sys_lock);
+  //lock_acquire(&sys_lock);
   file_read_at(spte->file, frame, spte->read_bytes, spte->ofs); 
-  lock_release(&sys_lock);
+  //lock_release(&sys_lock);
   //memset(frame+spte->read_bytes, 0, spte->zero_bytes);
   install_page(spte->page, frame, spte->writable);
   
