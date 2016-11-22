1a33fe4f3bb16254c34e9ee650f0de0ee397ea79
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-22 14:41:48 +0900

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -590,13 +590,11 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
      
-      //spt_alloc_lazy(&thread_current()->spt, upage, writable, PAL_USER|PAL_ZERO, page_read_bytes, page_zero_bytes, file, ofs);
-
-      /* Get a page of memory. */
+      spt_alloc_lazy(&thread_current()->spt, upage, writable, PAL_USER|PAL_ZERO, page_read_bytes, page_zero_bytes, file, ofs);
+      /**
       uint8_t *kpage = frame_spt_alloc( &thread_current()->spt, upage, writable, PAL_USER);
       if (kpage == NULL)
         return false;
-      /* Load this page. */
       if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
         {
           palloc_free_page (kpage);
@@ -604,19 +602,18 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
         }
       memset (kpage + page_read_bytes, 0, page_zero_bytes);
 
-      /* Add the page to the process's address space. */
       if (!install_page (upage, kpage, writable)) 
         {
           palloc_free_page (kpage);
           return false; 
-        }
+        }**/
       /*
      spt_alloc(&thread_current()->spt, upage);
       */
       /* Advance. */
       read_bytes -= page_read_bytes;
       zero_bytes -= page_zero_bytes;
-      //ofs += page_read_bytes;
+      ofs += page_read_bytes;
       upage += PGSIZE;
     }
   
