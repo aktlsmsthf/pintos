016d4c4e2e0010844ea240bb41c60a4b346d4d70
aktlsmsthf <lch7224@kaist.ac.kr>
2016-11-27 17:06:07 +0900

    Update frame.h

--- pintos/src/vm/frame.h
+++ pintos/src/vm/frame.h
@@ -13,13 +13,12 @@ struct frame_entry{
   void *frame;
   bool in_swap;
   size_t swap_where;
-  bool is_free;
   struct spt_entry * spte;
   struct list_elem elem;
 };
 
 void frame_init(void);
-void frame_remove(struct frame_entry *fe,bool pe);
+void frame_remove(struct frame_entry *fe);
 
 void * frame_spt_alloc(struct hash * spt, void * page, bool writable, enum palloc_flags flags);
 void * frame_evict(enum palloc_flags flags);
