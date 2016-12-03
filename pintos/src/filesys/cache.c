017cf7e1cdb5f37c534b8805763f336578903371
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-03 22:42:01 +0900

    Update cache.c

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -5,6 +5,20 @@ void cache_init(void){
   list_init(&cache_list);
 }
 
-int caching_index(void){
+bool exist_cache(int sector_idx){
+  struct list_elem *elem = list_front(&cache_list);
+  bool exist = false;
+  while(elem->next = NULL){
+    if(list_entry(elem, &cache_list, elem)->sector == sector_idx){
+      exist = true;
+    }
+  }
+  return exist;
+}
+
+void read_ahead(int sector_idx){
   
+}
+
+void write_behind(void){
 }
