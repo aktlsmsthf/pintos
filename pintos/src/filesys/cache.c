49dc79f6c66bdbab5ba8bd2a97253bc3144dfefa
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-04 17:50:54 +0900

    Update cache.c

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -14,7 +14,9 @@ void cache_init(void){
 
 struct cache_entry * find_cache_by_sector(int sector_idx){
   struct list_elem *elem = list_front(&cache_list);
-  
+  if(list_empty(&cache_list){
+    return NULL;
+  }
   lock_acquire(&cache_lock);
   while(elem->next != NULL){
     if(list_entry(elem, struct cache_entry, elem)->sector == sector_idx){
