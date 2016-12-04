82133b839696a289327a6dfc0fa38472446165dd
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-05 00:10:58 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -138,9 +138,9 @@ syscall_handler (struct intr_frame *f UNUSED)
           f->eax = -1;
         }
         else{
-          lock_acquire(&sys_lock);
+          //lock_acquire(&sys_lock);
           struct file *ff = filesys_open(name);
-          lock_release(&sys_lock);
+          //lock_release(&sys_lock);
           if(ff==NULL) {
             f->eax = -1;
           }
@@ -240,9 +240,9 @@ syscall_handler (struct intr_frame *f UNUSED)
           f->eax = -1;
         }
         else{
-          lock_acquire(&sys_lock);
+          //lock_acquire(&sys_lock);
           int r = (int) file_read(ff, buffer, size);
-          lock_release(&sys_lock);
+          //lock_release(&sys_lock);
           f->eax = r;
         }
       }
@@ -357,11 +357,11 @@ syscall_handler (struct intr_frame *f UNUSED)
           break;
         }
         if(!ffd->is_closed && ffd->file!=NULL){
-          lock_acquire(&sys_lock);
+          //lock_acquire(&sys_lock);
            file_close(ffd->file);
           
           ffd->is_closed=1;
-          lock_release(&sys_lock);
+          //lock_release(&sys_lock);
         }
       } 
       break;
