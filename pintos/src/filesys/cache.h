c17a8fc0055b16e17268da119c32cb2a16aa696e
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-04 16:58:28 +0900

    Update cache.h

--- pintos/src/filesys/cache.h
+++ pintos/src/filesys/cache.h
@@ -17,4 +17,9 @@ struct cache_entry{
   struct list_elem elem;
 };
 
+void cache_init(void);
+struct cache_entry read_to_cache(int sector_idx, bool first);
+void write_behind(struct cache_entry *c);
+void write_behind_all(void);
+
 #endif
