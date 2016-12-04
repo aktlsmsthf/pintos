eebedcf96439b7b3ce63fa5390d13ca923d8ccef
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-04 22:25:46 +0900

    Update cache.c

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -52,7 +52,7 @@ struct cache_entry * read_to_cache(int sector_idx, bool first){
       disk_write(filesys_disk, c->sector, c->cache);
     }
     count--;
-    //free(c->cache);
+    free(c->cache);
     
   }
   else{
