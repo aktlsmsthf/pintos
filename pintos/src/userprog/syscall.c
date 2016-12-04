04dbee3972d9aa5dca92abdf2ccd6c940f3a720a
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-05 02:56:34 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -97,9 +97,9 @@ syscall_handler (struct intr_frame *f UNUSED)
         f->eax =-1;
       }
       else{
-        lock_acquire(&sys_lock);
+        //lock_acquire(&sys_lock);
         f->eax = filesys_create (file,initial_size);
-        lock_release(&sys_lock);
+        //lock_release(&sys_lock);
       }
       break;
     }
@@ -110,9 +110,9 @@ syscall_handler (struct intr_frame *f UNUSED)
       
       const char *file = *((char **)(f->esp)+1);
       if(check_bad_ptr(f,(const void *)file)) {exit(-1);}
-      lock_acquire(&sys_lock);
+      //lock_acquire(&sys_lock);
       f->eax = filesys_remove (file);
-      lock_release(&sys_lock);
+      //lock_release(&sys_lock);
       break;
      }
       
@@ -138,9 +138,9 @@ syscall_handler (struct intr_frame *f UNUSED)
           f->eax = -1;
         }
         else{
-          lock_acquire(&sys_lock);
+          //lock_acquire(&sys_lock);
           struct file *ff = filesys_open(name);
-          lock_release(&sys_lock);
+          //lock_release(&sys_lock);
           if(ff==NULL) {
             f->eax = -1;
           }
@@ -240,9 +240,9 @@ syscall_handler (struct intr_frame *f UNUSED)
           f->eax = -1;
         }
         else{
-          lock_acquire(&sys_lock);
+          //lock_acquire(&sys_lock);
           int r = (int) file_read(ff, buffer, size);
-          lock_release(&sys_lock);
+          //lock_release(&sys_lock);
           f->eax = r;
         }
       }
@@ -322,9 +322,9 @@ syscall_handler (struct intr_frame *f UNUSED)
       int fd = *((int *)(f->esp)+1);
       unsigned position = *((unsigned *)(f->esp)+2);
       struct file *ff = get_file_from_fd(fd);
-      lock_acquire(&sys_lock);
+      //lock_acquire(&sys_lock);
       file_seek(ff, position);
-      lock_release(&sys_lock);
+      //lock_release(&sys_lock);
       break;
     }
       
@@ -357,11 +357,11 @@ syscall_handler (struct intr_frame *f UNUSED)
           break;
         }
         if(!ffd->is_closed && ffd->file!=NULL){
-          lock_acquire(&sys_lock);
+          //lock_acquire(&sys_lock);
            file_close(ffd->file);
           
           ffd->is_closed=1;
-          lock_release(&sys_lock);
+          //lock_release(&sys_lock);
         }
       } 
       break;
