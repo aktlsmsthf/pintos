bc27d19aa6a28eac959bda575b3001dab6c51a80
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-05 12:37:06 +0900

    Update swap.c

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -64,10 +64,10 @@ void swap_in(struct frame_entry *fe, enum palloc_flags flags){
   fe->swap_where = -1;
   fe->frame = frame;
   install_page(fe->spte->page, frame, fe->spte->writable);
-
+  
+  lock_release(&swap_lock);
   lock_acquire(&frame_lock);
   list_push_back(&frame_table, &fe->elem);
   lock_release(&frame_lock);
   
-  lock_release(&swap_lock);
 }
