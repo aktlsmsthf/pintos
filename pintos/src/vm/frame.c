57d25b0973fceb4d527f87658569a5dfb5c0ef75
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-05 22:22:38 +0900

    Update frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -71,7 +71,7 @@ void* frame_evict(enum palloc_flags flags){
   struct list_elem * frame_elem = list_front(&frame_table);
   struct frame_entry * fe;
   fe = list_entry(frame_elem, struct frame_entry, elem);
-  while(!pagedir_is_dirty(fe->spte->t->pagedir, fe->spte->page) || fe->frame == NULL 
+  while( fe->frame == NULL 
         || pagedir_is_accessed(fe->spte->t->pagedir ,fe->spte->page)){
       if(fe->frame != NULL){
         pagedir_set_accessed(fe->spte->t->pagedir ,fe->spte->page, false);
