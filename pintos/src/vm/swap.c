86164aa7e7d3fd589cbc3b64042d36d8c27a97d4
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-05 13:05:32 +0900

    Update swap.c

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -53,11 +53,11 @@ void swap_in(struct frame_entry *fe, enum palloc_flags flags){
   while(frame == NULL){ frame = frame_evict(flags);}
   int i;
   size_t index = fe->swap_where;
-  
+  /*
   lock_acquire(&frame_lock);
   list_push_back(&frame_table, &fe->elem);
   lock_release(&frame_lock);
-  
+  */
   lock_acquire(&swap_lock);
   if (bitmap_test(swap_table, index) == 0){ return;}
   bitmap_flip(swap_table, index);
