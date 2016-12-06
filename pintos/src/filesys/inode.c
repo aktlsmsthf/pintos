22683c70b60b95a72d1a33fccef09bf5f2cb84d9
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-06 23:14:52 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -153,8 +153,8 @@ inode_create (disk_sector_t sector, off_t length)
               }
            }
            disk_inode->indirect++;
-           free_map_allocate(1, &disk_inode[i]);
-           disk_write(filesys_disk, disk_inode[i], sector);
+           free_map_allocate(1, &disk_inode->indirect_sector[i]);
+           disk_write(filesys_disk, disk_inode->indirect_sector[i], sector);
            if(sectors==0){
               break;
            }
