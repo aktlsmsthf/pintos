7464d095ee5b3b5f79a92ebd0fd80e48345d868a
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-06 23:40:01 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -103,6 +103,7 @@ inode_create (disk_sector_t sector, off_t length)
 
   /* If this assertion fails, the inode structure is not exactly
      one sector in size, and you should fix that. */
+   printf("%d\n", sizeof *disk_inode);
   ASSERT (sizeof *disk_inode == DISK_SECTOR_SIZE);
 
   disk_inode = calloc (1, sizeof *disk_inode);
