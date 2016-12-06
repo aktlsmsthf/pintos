bc8e9458d3a30658bb201418d16e009f2ce04739
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-07 06:11:13 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -403,7 +403,7 @@ inode_write_at (struct inode *inode, const void *buffer_, off_t size,
       disk_sector_t sectors = bytes_to_sectors(inode->data.length);
       disk_sector_t sectors2 = bytes_to_sectors(size+offset);
       static char zeros[DISK_SECTOR_SIZE];
-      while(sectors<sectors2){
+      while(sectors!=sectors2){
          sectors++;
          if(sectors<10){
             free_map_allocate(1, &inode->data.direct_sector[sectors]);
