2845ba695d6b20fef82aa5f401f8a8761f1e347c
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-07 19:55:58 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -465,14 +465,17 @@ inode_write_at (struct inode *inode, const void *buffer_, off_t size,
       if (chunk_size <= 0)
         break;
        
+     printf("a\n");
       
       struct cache_entry *c = find_cache_by_sector(sector_idx);
        if(!c){
           c = read_to_cache(sector_idx, true);
        }
+     printf("b\n");
        //lock_acquire(&cache_lock);
        memcpy(c->cache+sector_ofs, buffer+bytes_written, chunk_size);
        c->dirty = true;
+     printf("c\n");
        //lock_release(&cache_lock);
        /**default
       if (sector_ofs == 0 && chunk_size == DISK_SECTOR_SIZE) 
