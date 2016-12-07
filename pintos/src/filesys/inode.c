076a46fa7e88e9f162f0338a1b9c7c6a90f20e44
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-07 21:13:42 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -138,7 +138,6 @@ inode_create (disk_sector_t sector, off_t length)
            disk_sector_t sector[128];
            int j;
            for(j=0; j<128; j++){
-              printf("1\n");
               free_map_allocate(1, &sector[j]);
               disk_write(filesys_disk, sector[j], zeros);
               if(--sectors==0){
