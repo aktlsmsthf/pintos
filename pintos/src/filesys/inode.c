ddab4ff88506d12a5ab821f5a4450651776c12c4
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-07 22:18:20 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -380,6 +380,7 @@ inode_write_at (struct inode *inode, const void *buffer_, off_t size,
    
    if(size+offset>inode->data.length){
       //inode_deny_write (inode); 
+      lock_acquire(&inode_lock);
       disk_sector_t sectors = bytes_to_sectors(inode->data.length);
       disk_sector_t sectors2 = bytes_to_sectors(size+offset);
       static char zeros[DISK_SECTOR_SIZE];
@@ -420,7 +421,7 @@ inode_write_at (struct inode *inode, const void *buffer_, off_t size,
       }
       inode->data.length = size+offset;
       disk_write(filesys_disk, inode->data.sector, &inode->data);
-      
+      lock_release(&inode_lock);
       //inode_allow_write (inode);
    }
    
