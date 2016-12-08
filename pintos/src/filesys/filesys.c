7751b28d79f2f3d72880b36ddecbc82db66637a6
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-09 04:25:54 +0900

    Update filesys.c

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -47,15 +47,25 @@ filesys_done (void)
    Fails if a file named NAME already exists,
    or if internal memory allocation fails. */
 bool
-filesys_create (const char *name, off_t initial_size) 
+filesys_create (const char *name, off_t initial_size, bool is_dir) 
 {
   disk_sector_t inode_sector = 0;
   //struct dir *dir = dir_open_root ();
-  char *dir_name;
-  bool success = (dir != NULL
+  char *real_name;
+  struct dir *dir = lowest_dir(name, &real_name);
+  bool success;
+  if(is_dir){
+     success= (dir!=NULL
+                    && free_map_allocate(1, &inode_sector)
+                    && dir_create(inode_sector, 16);
+                    && dir_add(dir, real_name));
+  }
+   else{
+       bool success = (dir != NULL
                   && free_map_allocate (1, &inode_sector)
                   && inode_create (inode_sector, initial_size)
                   && dir_add (dir, name, inode_sector));
+   }
   if (!success && inode_sector != 0) 
     free_map_release (inode_sector, 1);
   dir_close (dir);
