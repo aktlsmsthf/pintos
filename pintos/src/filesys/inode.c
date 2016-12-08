ef0c019b088b71ef20ada9d96cf300480c40813e
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-08 09:14:40 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -128,7 +128,7 @@ inode_create (disk_sector_t sector, off_t length)
       disk_inode->sector = sector;
 
      
-      disk_sector_t sectors = 0;
+      disk_sector_t sectors = -1;
       disk_sector_t sectors2 = bytes_to_sectors(length);
       
       lock_acquire(&inode_lock);
