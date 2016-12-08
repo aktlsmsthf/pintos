5ac620224f0756022d1b4faf13bb7be330745297
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-09 04:14:10 +0900

    Update directory.h

--- pintos/src/filesys/directory.h
+++ pintos/src/filesys/directory.h
@@ -26,5 +26,6 @@ bool dir_lookup (const struct dir *, const char *name, struct inode **);
 bool dir_add (struct dir *, const char *name, disk_sector_t);
 bool dir_remove (struct dir *, const char *name);
 bool dir_readdir (struct dir *, char name[NAME_MAX + 1]);
+struct dir *lowest_dir(char *name, char **dir_name);
 
 #endif /* filesys/directory.h */
