44b2515fd3a82bea01e33884677f6b60ac5cd2a0
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-08 09:58:59 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -61,7 +61,7 @@ byte_to_sector (const struct inode *inode, off_t pos)
   else
     return -1;**/
    
-   int sectors = pos/DISK_SECTOR_SIZE;
+   off_t sectors = pos/DISK_SECTOR_SIZE;
    if(pos>=inode->data.length){
       
       return -1;
