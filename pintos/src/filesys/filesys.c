c406d164dc811a62ad54cc1274dfb9f6ea0e1d4d
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-10 03:13:34 +0900

    Update filesys.c

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -161,7 +161,7 @@ filesys_remove (const char *name)
         rdir = dir;
         dir = dir_open(inode_open(inode_parent(dir_get_inode(dir))));
      }
-     else if(strcmp(real_name, ".") || strcmp(real_name, ".."){
+     else if(strcmp(real_name, ".") || strcmp(real_name, "..")){
         rdir = dir;
         dir = dir_open(inode_open(inode_parent(dir_get_inode(dir))));
      }
