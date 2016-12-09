bada04fb14c235d0dc5f7c0305d7e66bd0cfe242
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-09 16:34:45 +0900

    Update directory.c

--- pintos/src/filesys/directory.c
+++ pintos/src/filesys/directory.c
@@ -243,7 +243,7 @@ struct dir * lowest_dir(char *name, char **dir_name){
    token =name;
    struct dir *dir;
    struct dir *temp;
-   if(name[0] == "/"){
+   if(name[0] == "/" || thread_current()->current_dir==NULL){
       dir = dir_open_root();
       token = strtok_r(name, "/", &save);
    }
