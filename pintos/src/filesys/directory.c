62b478042ef2e860dd509f8f1fb235395d7da36e
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-09 22:47:36 +0900

    Update directory.c

--- pintos/src/filesys/directory.c
+++ pintos/src/filesys/directory.c
@@ -252,6 +252,8 @@ struct dir * lowest_dir(char *name, char **dir_name){
       dir = dir_reopen(thread_current()->current_dir);
    }
    printf("%s\n", name);
+
+  printf("%s\n", strtok_r("a/n", "/", &save);
    token = strtok_r(name, "/", &save);
   printf("2\n");
    while(strtok_r(NULL, "/", &save)!=NULL){
