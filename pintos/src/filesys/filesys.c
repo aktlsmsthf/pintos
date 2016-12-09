9427d92324de3d1c1d61350ecc03e8998b394540
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-10 07:45:07 +0900

    Update filesys.c

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -111,7 +111,6 @@ filesys_open (const char *name)
     }
     dir_lookup (dir, real_name, &inode);
   }
-  dir_close (dir);
   if(inode == NULL || inode_is_dir(inode)){
      return NULL;
   }
@@ -136,6 +135,7 @@ filesys_open_dir(const char *name){
     
   }
   
+  dir_close (dir);
   if(inode == NULL || !inode_is_dir(inode)){
      //printf("%d %s\n", get_sector_dir(dir), real_name);
      return NULL;
