20015f4842e952563b013f67cfa0970a966ada35
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-10 00:11:05 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -523,8 +523,9 @@ syscall_handler (struct intr_frame *f UNUSED)
       if(!user_memory(f->esp, 1)){ exit(-1);}
       char * dir = *((char **)(f->esp)+1);	
       if(check_bad_ptr(f, dir)) exit(-1);
+      char *e = ""
 	  printf("%s\n", dir);
-      if(dir==NULL || dir ==""){
+      if(dir==NULL || strcmp(dir,"")==0){
 	      printf("1\n");
 	      f->eax = false;
 	      break;
