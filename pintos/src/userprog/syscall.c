991c3b51e48170cc18ab7c9d91d1b0ff433785fc
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-10 04:58:05 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -147,12 +147,12 @@ syscall_handler (struct intr_frame *f UNUSED)
 	    lock_acquire(&sys_lock);
             struct file *file = filesys_open(name);
 	    struct dir *dir = filesys_open_dir(name);
+            lock_release(&sys_lock);
+	    	
 	    if(dir==NULL && file==NULL){
 		    f->eax = -1;
-		    lock_release(&sys_lock);
 		    break;
 	    }
-            lock_release(&sys_lock);
             struct thread *t = thread_current();
             struct file_fd *ffd = palloc_get_page(0);
             if(ffd==NULL){
