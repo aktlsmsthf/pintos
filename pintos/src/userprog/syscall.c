6e415a94ebcb314c8d88d7cefc85a5909d24ccd5
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-11 00:59:54 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -155,7 +155,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 		    
             struct thread *t = thread_current();
             struct file_fd *ffd = palloc_get_page(0);
-		printf("%d\n", sizeof(struct file_fd));
+		printf("%d\n", sizeof(ffd));
             if(ffd==NULL){
               f->eax =-1;
             }
