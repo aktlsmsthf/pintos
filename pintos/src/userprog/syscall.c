a0c8b273290ff0724bfa6cb65572f82ba302c62a
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-11 02:44:13 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -527,7 +527,6 @@ syscall_handler (struct intr_frame *f UNUSED)
 		dir = dir_open(inode);
 	}
 	if(dir!=NULL){
-		pritntf("%d\n",get_sector_dir(dir));
 		dir_close(thread_current()->current_dir);
 		thread_current()->current_dir = dir;
 		f->eax = true;
