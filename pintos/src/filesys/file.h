8236446b7a81ac4d288f6293addc093dab4bda46
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-10 23:26:51 +0900

    Update file.h

--- pintos/src/filesys/file.h
+++ pintos/src/filesys/file.h
@@ -2,6 +2,7 @@
 #define FILESYS_FILE_H
 
 #include "filesys/off_t.h"
+#include "devices/disk.h"
 
 struct inode;
 
@@ -25,6 +26,6 @@ void file_allow_write (struct file *);
 void file_seek (struct file *, off_t);
 off_t file_tell (struct file *);
 off_t file_length (struct file *);
-disk_sector_t get_sector_file(struct file*);
+disk_sector_t get_sector_file(struct file *);
 
 #endif /* filesys/file.h */
