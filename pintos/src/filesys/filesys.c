41dde41f6a9d59b8c75e6e553cb6b66295b00a0e
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-10 16:02:10 +0900

    Update filesys.c

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -184,6 +184,7 @@ filesys_remove (const char *name)
    }
    if(get_sector_dir(rdir) == ROOT_DIR_SECTOR){
       success == false;
+      printf("%d\n", success);
    }
    else{
       if(thread_current()->current_dir !=NULL){
