4275565523a051eb8ef75d1ad8146cb4ae29fd3b
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-10 14:56:59 +0900

    Update directory.c

--- pintos/src/filesys/directory.c
+++ pintos/src/filesys/directory.c
@@ -245,7 +245,7 @@ struct dir * lowest_dir(char *name, char **dir_name){
    struct dir *dir;
    struct dir *temp;
    char slash = "/";
-   printf("%d\n", strcmp(name[0], slash));
+   printf("%d\n", name[0]==slash);
    if(name[0] == "/" || thread_current()->current_dir == NULL ){
      printf("a\n");
       dir = dir_open_root();
