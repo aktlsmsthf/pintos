eba4b96506667f2a9be938bae36e7bd3d28d97a1
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-10 16:38:38 +0900

    Update filesys.c

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -102,17 +102,19 @@ filesys_open (const char *name)
   dir = lowest_dir(name, &real_name);
   if (dir != NULL){
      if(real_name ==NULL){
+        printf("1\n");
         dir_close(dir);
         return NULL;
      }
      if(strcmp(real_name, ".") ==0 || strcmp(real_name, "..")==0){
+        printf("2\n");
        dir_close(dir);
         return NULL;
     }
     dir_lookup (dir, real_name, &inode);
      
   }
-  
+  printf("3\n");
   dir_close (dir); 
   if(inode == NULL || inode_is_dir(inode)){
      return NULL;
