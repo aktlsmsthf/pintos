3b12ce14315861f1f706dcb95acd8b608ba274e8
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-12 03:02:21 +0900

    Update directory.c

--- pintos/src/filesys/directory.c
+++ pintos/src/filesys/directory.c
@@ -125,12 +125,12 @@ dir_lookup (const struct dir *dir, const char *name,
   ASSERT (dir != NULL);
   ASSERT (name != NULL);
 
-  //ilock_acquire(dir_get_inode(dir));
+  ilock_acquire(dir_get_inode(dir));
   if (lookup (dir, name, &e, NULL))
     *inode = inode_open (e.inode_sector);
   else
     *inode = NULL;
-  //ilock_release(dir_get_inode(dir));
+  ilock_release(dir_get_inode(dir));
   return *inode != NULL;
 }
 
@@ -150,7 +150,7 @@ dir_add (struct dir *dir, const char *name, disk_sector_t inode_sector)
   ASSERT (dir != NULL);
   ASSERT (name != NULL);
 
-  //ilock_acquire(dir_get_inode(dir));
+  ilock_acquire(dir_get_inode(dir));
   /* Check NAME for validity. */
   if (*name == '\0' || strlen (name) > NAME_MAX)
     return false;
@@ -179,7 +179,7 @@ dir_add (struct dir *dir, const char *name, disk_sector_t inode_sector)
   success = inode_write_at (dir->inode, &e, sizeof e, ofs) == sizeof e;
 
  done:
-  //ilock_release(dir_get_inode(dir));
+  ilock_release(dir_get_inode(dir));
   return success;
 }
 
@@ -197,7 +197,7 @@ dir_remove (struct dir *dir, const char *name)
   ASSERT (dir != NULL);
   ASSERT (name != NULL);
 
-  //ilock_acquire(dir_get_inode(dir));
+  ilock_acquire(dir_get_inode(dir));
   /* Find directory entry. */
   if (!lookup (dir, name, &e, &ofs))
     goto done;
@@ -218,7 +218,7 @@ dir_remove (struct dir *dir, const char *name)
 
  done:
   inode_close (inode);
-  //ilock_release(dir_get_inode(dir));
+  ilock_release(dir_get_inode(dir));
   return success;
 }
 
@@ -230,7 +230,7 @@ dir_readdir (struct dir *dir, char name[NAME_MAX + 1])
 {
   struct dir_entry e;
 
-  //ilock_acquire(dir_get_inode(dir));
+  ilock_acquire(dir_get_inode(dir));
   while (inode_read_at (dir->inode, &e, sizeof e, dir->pos) == sizeof e) 
     {
       dir->pos += sizeof e;
@@ -241,7 +241,7 @@ dir_readdir (struct dir *dir, char name[NAME_MAX + 1])
           return true;
         } 
     }
-  //ilock_release(dir_get_inode(dir));
+  ilock_release(dir_get_inode(dir));
   return false;
 }
 
