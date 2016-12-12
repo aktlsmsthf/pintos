31950e1976799864b26c1c2a434467e70dee5ca2
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-12 16:45:30 +0900

    Update cache.c

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -11,7 +11,7 @@
 void cache_init(void){
   list_init(&cache_list);
   lock_init(&cache_lock);
-  //thread_create("Write_behind_periodically", 0, thread_func_write_behind, NULL);
+  thread_create("Write_behind_periodically", 0, thread_func_write_behind, NULL);
   count = 0;
 }
 
@@ -82,10 +82,10 @@ struct cache_entry * read_to_cache(int sector_idx, bool first){
   
   disk_read(filesys_disk, sector_idx, c->cache);
   lock_release(&cache_lock);
-  /**if(first){
+  if(first){
     void *aux = sector_idx+1;
     thread_create("Read_ahead", 0, thread_func_read_ahead, aux);
-  }**/
+  }
   
   return c;
 }
