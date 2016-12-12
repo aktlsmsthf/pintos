bac605f8a0c1fbccf405e4e999633e23c5b1c814
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-12 23:34:38 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -1,20 +1,3 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Gist
- @aktlsmsthf
- Watch 0
-  Star 0
-  Fork 1 aktlsmsthf/pintos
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs  Settings
-Tree: f777bd85f4 Find file Copy pathpintos/pintos/src/userprog/syscall.c
-f777bd8  an hour ago
-@aktlsmsthf aktlsmsthf Update syscall.c
-1 contributor
-RawBlameHistory     
-660 lines (588 sloc)  16.2 KB
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
