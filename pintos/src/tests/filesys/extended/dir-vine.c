92e59f48916ee9b8b98b5ea8499d890d0deda568
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-12 20:14:53 +0900

    Update dir-vine.c

--- pintos/src/tests/filesys/extended/dir-vine.c
+++ pintos/src/tests/filesys/extended/dir-vine.c
@@ -36,7 +36,7 @@ test_main (void)
       CHECK ((fd = open (file_name)) > 1, "open \"%s\"", file_name);
       snprintf (contents, sizeof contents, "contents %d\n", i);
       if (write (fd, contents, strlen (contents)) != (int) strlen (contents)) 
-        {for(;;);
+        {
           CHECK (remove (file_name), "remove \"%s\"", file_name);
           close (fd);
           break;
