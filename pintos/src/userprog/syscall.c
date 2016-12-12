a9478aa539836ff2d2d7afd93a2a603455c4147f
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-12 22:37:54 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -100,9 +100,9 @@ syscall_handler (struct intr_frame *f UNUSED)
         f->eax = false;
       }
       else{
-        lock_acquire(&sys_lock);
+        //lock_acquire(&sys_lock);
         f->eax = filesys_create (file,initial_size);
-        lock_release(&sys_lock);
+        //lock_release(&sys_lock);
       }
       break;
     }
@@ -113,9 +113,9 @@ syscall_handler (struct intr_frame *f UNUSED)
       
       const char *file = *((char **)(f->esp)+1);
       if(check_bad_ptr(f,(const void *)file)) {exit(-1);}
-      lock_acquire(&sys_lock);
+      //lock_acquire(&sys_lock);
       f->eax = filesys_remove (file);
-      lock_release(&sys_lock);
+      //lock_release(&sys_lock);
       break;
      }
       
@@ -141,10 +141,10 @@ syscall_handler (struct intr_frame *f UNUSED)
 	  break;
         }
         else{   
-            lock_acquire(&sys_lock);
+            //lock_acquire(&sys_lock);
             struct file *file = filesys_open(name);
 	    struct dir *dir = filesys_open_dir(name);
-            lock_release(&sys_lock);
+            //lock_release(&sys_lock);
 	    if(dir==NULL && file==NULL){
 		    f->eax = -1;
 		    break;
@@ -256,9 +256,9 @@ syscall_handler (struct intr_frame *f UNUSED)
           f->eax = -1;
         }
         else{
-          lock_acquire(&sys_lock);
+          //lock_acquire(&sys_lock);
           int r = (int) file_read(ff, buffer, size);
-          lock_release(&sys_lock);
+          //lock_release(&sys_lock);
           f->eax = r;
         }
       }
@@ -334,9 +334,9 @@ syscall_handler (struct intr_frame *f UNUSED)
           f->eax = -1;
         }
         else{
-          lock_acquire(&sys_lock);
+          //lock_acquire(&sys_lock);
           int r = (int) file_write(ff, buffer, size);
-          lock_release(&sys_lock);
+          //lock_release(&sys_lock);
           f->eax = r;
         }
       }
@@ -350,9 +350,9 @@ syscall_handler (struct intr_frame *f UNUSED)
       int fd = *((int *)(f->esp)+1);
       unsigned position = *((unsigned *)(f->esp)+2);
       struct file *ff = get_file_from_fd(fd);
-      lock_acquire(&sys_lock);
+      //lock_acquire(&sys_lock);
       file_seek(ff, position);
-      lock_release(&sys_lock);
+      //lock_release(&sys_lock);
       break;
     }
       
@@ -414,7 +414,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 		f->eax = -1;
 		break;
 	}
-	lock_acquire(&sys_lock);   
+	//lock_acquire(&sys_lock);   
 	struct file *mfile = file_reopen(file);
 	
 	//lock_release(&sys_lock);   
@@ -456,7 +456,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 	else{
 		f->eax = -1;
 	}  
-	lock_release(&sys_lock);
+	//lock_release(&sys_lock);
 	break;
     }
     
@@ -473,10 +473,10 @@ syscall_handler (struct intr_frame *f UNUSED)
 		if(!spte->lazy){
 			if(pagedir_is_dirty(spte->t->pagedir, addr)){
 				
-				lock_acquire(&sys_lock);  
+				//lock_acquire(&sys_lock);  
 				file_write_at(mapped->file, spte->page, spte->read_bytes, spte->ofs);
 				
-				lock_release(&sys_lock);  
+				//lock_release(&sys_lock);  
 			}
 		}
 		pagedir_clear_page(spte->t, addr);
@@ -489,9 +489,9 @@ syscall_handler (struct intr_frame *f UNUSED)
 		write_bytes+=PGSIZE;
 	}
 	  
-	lock_acquire(&sys_lock);
+	//lock_acquire(&sys_lock);
 	list_remove(&mapped->elem);
-	 lock_release(&sys_lock);   
+	 //lock_release(&sys_lock);   
 	file_close(mapped->file);
 	 
 	free(mapped);
@@ -522,9 +522,9 @@ syscall_handler (struct intr_frame *f UNUSED)
 	}
 	if(dir!=NULL){
 		dir_close(thread_current()->current_dir);
-		lock_acquire(&sys_lock);
+		//lock_acquire(&sys_lock);
 		thread_current()->current_dir = dir;
-		lock_release(&sys_lock);
+		//lock_release(&sys_lock);
 		f->eax = true;
 	}
 	else{
@@ -541,9 +541,9 @@ syscall_handler (struct intr_frame *f UNUSED)
 	      f->eax = false;
 	      break;
       } 
-	lock_acquire(&sys_lock);
+	//lock_acquire(&sys_lock);
 	f->eax = filesys_create_dir(dir);
-	lock_release(&sys_lock);
+	//lock_release(&sys_lock);
       break;
     }
     case SYS_READDIR:{
