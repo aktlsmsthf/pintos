72c43435796d0f800cd11a6cc7541eee422ffbe3
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-13 01:15:39 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -209,8 +209,9 @@ inode_open (disk_sector_t sector)
 
   /* Allocate memory. */
   inode = malloc (sizeof *inode);
-  if (inode == NULL)
+  if (inode == NULL){printf("1\n");
     return NULL;
+  }
 
   /* Initialize. */
   list_push_front (&open_inodes, &inode->elem);
