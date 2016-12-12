42c14bf795e182601748cd27eb69dee2b3d0dec4
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-12 23:26:44 +0900

    Update syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -365,9 +365,9 @@ syscall_handler (struct intr_frame *f UNUSED)
         f->eax = -1;
       }
       else{
-        //lock_acquire(&sys_lock);
+        lock_acquire(&sys_lock);
         f->eax = file_tell(ff);
-        //lock_release(&sys_lock);
+        lock_release(&sys_lock);
       }
       break;
     }
