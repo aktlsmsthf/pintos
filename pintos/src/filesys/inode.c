42955931ddb23a063ef98e4e913acfcbc09b2949
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-12 20:37:06 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -139,7 +139,7 @@ inode_create (disk_sector_t sector, off_t length, bool is_dir)
       
       lock_acquire(&inode_lock);
       success = inode_extension(disk_inode, sectors, sectors2);
-      printf("1\n");
+      printf("%d\n", success);
       /**static char zeros[DISK_SECTOR_SIZE];  
       while(sectors!=sectors2){
          sectors++;
