f17eed836a901166bdf70ac9c0210d10c37b83a8
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-12 16:58:01 +0900

    Update cache.c

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -92,7 +92,7 @@ struct cache_entry * read_to_cache(int sector_idx, bool first){
 }
 
 void thread_func_read_ahead(void *aux){
-  int idx = *aux;
+  int idx = *(int *)aux;
   printf("b %d\n", idx);
   read_to_cache(idx, false);
 }
