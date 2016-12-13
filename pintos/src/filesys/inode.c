271b317c145d5ef8e4288f689344415f2797d7dc
aktlsmsthf <lch7224@kaist.ac.kr>
2016-12-13 14:56:50 +0900

    Update inode.c

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -333,7 +333,7 @@ inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset)
   off_t bytes_read = 0;
   uint8_t *bounce = NULL;
   //lock_acquire(&inode_lock);
-
+  off_t next = size+offset;
   while (size > 0) 
     {
       /* Disk sector to read, starting byte offset within sector. */
@@ -363,7 +363,11 @@ inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset)
       offset += chunk_size;
       bytes_read += chunk_size;
     }
-     
+    if(next+DISK_SECTOR_SIZE<inode->data.length){
+       struct sector_data sd;
+       sd.next_sector = byte_to_sector(next+DISK_SECTOR_SIZE);
+       thread_create("read_ahead", 0, thread_func_read_ahead, &se);
+    }
   //lock_release(&inode_lock);
   free (bounce);
 
